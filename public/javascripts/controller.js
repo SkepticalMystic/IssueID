4f5fadd47c52f4a74b897732454f4942fdec16a3
SkepticalMystic <dryvin@gmail.com>
2016-04-30 15:41:15 -0500

    Corrections

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,15 +1,8 @@
 var app = angular.module('IssueID', ['ngMaterial'])
-<<<<<<< HEAD
+
 app.controller('issueCtrl', function($scope, $http) {
     $http.get("/issues").then(function(response) {
 	$scope.myIssues = response.data.issues;
     });
 });
 
-
-
-=======
-    app.controller('issueCtrl', function($scope) {
-	$scope.myIssues = JSON.stringify(issues);
-});
->>>>>>> 393786544b1c31f9eabcbc98462d78bb2ae07bb1
